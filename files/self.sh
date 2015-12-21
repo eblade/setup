d957320e70ed026c1eeed9088721f3f5cd306141
Johan Egneblad <johan@egneblad.se>
2015-12-21 14:36:55 +0100

    Added autocompletion for prof and require

--- files/self.sh
+++ files/self.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-PATH="$HOME/git/setup:$PATH"
+SETUP_PATH="$HOME/git/setup"
+
+PATH="$SETUP_PATH:$PATH"
+
+function _profComplete {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$(ls $SETUP_PATH/profiles/)" -- $cur) )
+}
+
+complete -F _profComplete prof
+
+function _requireComplete {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$(ls $SETUP_PATH/components/)" -- $cur) )
+}
+
+complete -F _requireComplete require
