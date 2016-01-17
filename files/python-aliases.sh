0f1e491a5f0a39998f5c78bd41abcfc823595103
Johan Egneblad <johan@egneblad.se>
2016-01-17 18:42:11 +0100

    Auto-completion for source activate alias sa

--- files/python-aliases.sh
+++ files/python-aliases.sh
@@ -3,3 +3,11 @@
 alias pt="tree -I __pycache__ -I \*.pyc"
 alias p.="PYTHONPATH=\$(pwd) python"
 alias sa="source activate"
+
+function _saComplete {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$(ls $HOME/miniconda3/envs/)" -- $cur) )
+}
+
+complete -F _saComplete sa
+
