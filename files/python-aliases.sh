c56bb15287d2e92b608fcaf1d738c312bc966fa1
Johan Egneblad <johan@egneblad.se>
2016-02-10 13:58:24 +0100

    Support miniconda 2 and 3

--- files/python-aliases.sh
+++ files/python-aliases.sh
@@ -6,7 +6,11 @@ alias sa="source activate"
 
 function _saComplete {
     local cur=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(compgen -W "$(ls $HOME/miniconda3/envs/)" -- $cur) )
+    if [[ -d "$HOME/miniconda3" ]]; then
+        COMPREPLY=( $(compgen -W "$(ls $HOME/miniconda3/envs/)" -- $cur) )
+    elif [[ -d "$HOME/miniconda2" ]]; then
+        COMPREPLY=( $(compgen -W "$(ls $HOME/miniconda2/envs/)" -- $cur) )
+    fi
 }
 
 complete -F _saComplete sa
