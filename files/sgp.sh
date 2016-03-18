b120f73d3b363bdf8b445ae4b111b4c9ca81f54d
Johan Egneblad <johan@egneblad.se>
2016-03-18 10:52:23 +0000

    SGP password self-destruction

--- files/sgp.sh
+++ files/sgp.sh
@@ -2,4 +2,8 @@
 
 function pass {
     PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1 | xclip -selection clipboard
+    echo "This password will self-destruct in 10 seconds..."
+    sleep 10
+    echo "pooff" | xclip -selection clipboard
+    echo "Pooff"
 }
