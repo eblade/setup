adb63470453751df91bb2b5b43ad73f8e6b9f522
Johan Egneblad <johan@egneblad.se>
2016-04-04 09:42:22 +0100

    rt: aliases

--- files/bashrc-vizrt.sh
+++ files/bashrc-vizrt.sh
@@ -8,3 +8,9 @@ export ONEHOST=one-dev-sles
 
 export RT_ROOT=/home/jne/time_reports
 export PERL5LIB=/opt/ardome/lib/perl:$PERL5LIB
+
+alias current="vim $HOME/time_reports/current"
+alias lt="rt ls"
+alias month="rt dig"
+alias punch="rt punch"
+alias lunch="rt break"
