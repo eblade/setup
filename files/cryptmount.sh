bcbc38b096e7a8c8a1d6b4a91be621af58cfc74a
Johan Egneblad <johan@egneblad.se>
2016-11-27 00:17:25 +0000

    Add cryptmount scripts

---
+++ files/cryptmount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH="$HOME/git/cryptmount/bin:$PATH"
+export CRYPTMOUNT_PROFILES="$SETUP_PATH/files/cryptmount"
+
+function _cryptmountComplete {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$(ls $CRYPTMOUNT_PROFILES)" -- $cur) )
+}
+
+complete -F _cryptmountComplete cmount
+complete -F _cryptmountComplete cumount
