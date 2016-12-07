55852be13f24d22be58efff760771a249d06c53e
Johan Egneblad <johan@egneblad.se>
2016-12-07 16:12:05 +0100

    Include all anaconda/miniconda possibilities

--- files/python-aliases.sh
+++ files/python-aliases.sh
@@ -8,6 +8,10 @@ function _saComplete {
     local cur=${COMP_WORDS[COMP_CWORD]}
     if [[ -d "$HOME/anaconda3" ]]; then
         COMPREPLY=( $(compgen -W "$(ls $HOME/anaconda3/envs/)" -- $cur) )
+    elif [[ -d "$HOME/anaconda2" ]]; then
+        COMPREPLY=( $(compgen -W "$(ls $HOME/anaconda2/envs/)" -- $cur) )
+    elif [[ -d "$HOME/miniconda3" ]]; then
+        COMPREPLY=( $(compgen -W "$(ls $HOME/miniconda3/envs/)" -- $cur) )
     elif [[ -d "$HOME/miniconda2" ]]; then
         COMPREPLY=( $(compgen -W "$(ls $HOME/miniconda2/envs/)" -- $cur) )
     fi
