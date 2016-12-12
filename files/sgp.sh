8aa065318b05a2db00ddbee357702d598da4f203
Johan Egneblad <johan@egneblad.se>
2016-12-12 11:49:59 +0100

    Merge, but keep windows changes here

--- files/sgp.sh
+++ files/sgp.sh
@@ -3,3 +3,7 @@
 function pass {
     PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1
 }
+
+function pass15 {
+    PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q -l 15 $1
+}
