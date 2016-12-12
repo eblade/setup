fd5efbb2e3ffece25beca9f2a7a3f5cf6a374f5e
Johan Egneblad <johan@egneblad.se>
2016-12-12 13:11:22 +0100

    Fix pass command to be xclip agnostic

--- files/sgp.sh
+++ files/sgp.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 function pass {
-    PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1
+    if (system("which xclip")==0); then
+        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1 | xclip -selection clipboard
+    else
+        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1
+    fi
 }
 
 function pass15 {
-    PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q -l 15 $1
+    if (system("which xclip")==0); then
+        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q -l 15 $1 | xclip -selection clipboard
+    else
+        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q -l 15 $1
+    fi
 }
