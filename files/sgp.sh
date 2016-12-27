0056ef44e44ba3e2a438801d9d10ba09c89f436f
Johan Egneblad <johan@egneblad.se>
2016-12-27 10:42:35 +0100

    Fix bad bash syntax

--- files/sgp.sh
+++ files/sgp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function pass {
-    if (system("which xclip")==0); then
-        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1 | xclip -selection clipboard
+    if [[ $(which xclip) == "0" ]]; then
+        PYTHONPATH="$HOME/git/supergenpass/core" python $HOME/git/supergenpass/core/sgp -q $1 | xclip -selection clipboard
     else
-        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q $1
+        PYTHONPATH="$HOME/git/supergenpass/core" python $HOME/git/supergenpass/core/sgp -q $1
     fi
 }
 
 function pass15 {
-    if (system("which xclip")==0); then
-        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q -l 15 $1 | xclip -selection clipboard
+    if [[ $(which xclip) == "0" ]]; then
+        PYTHONPATH="$HOME/git/supergenpass/core" python $HOME/git/supergenpass/core/sgp -q -l 15 $1 | xclip -selection clipboard
     else
-        PYTHONPATH="$HOME/git/supergenpass/core" python2 $HOME/git/supergenpass/core/sgp -q -l 15 $1
+        PYTHONPATH="$HOME/git/supergenpass/core" python $HOME/git/supergenpass/core/sgp -q -l 15 $1
     fi
 }
