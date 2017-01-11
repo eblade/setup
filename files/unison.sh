452bfde345735b5f0cef4de5ac9a134713c22b0b
Johan Egneblad <johan@egneblad.se>
2017-01-11 20:52:29 +0000

    Fix unison autocomplete to point to actual config folder

--- files/unison.sh
+++ files/unison.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-export UNISON_PROFILES="$SETUP_PATH/files/unison"
-
 _unisonComplete() {
     local cur=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(compgen -W "$(for F in $UNISON_PROFILES/*.prf; do echo $(basename ${F%.prf}); done)" -- $cur) )
+    COMPREPLY=( $(compgen -W "$(for F in $HOME/.unison/*.prf; do echo $(basename ${F%.prf}); done)" -- $cur) )
 }
 
 complete -F _unisonComplete unison
