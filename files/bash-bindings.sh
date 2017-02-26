8753aa0929c81d0d99909ea04127a5926616f99e
Johan Egneblad <johan@egneblad.se>
2017-02-26 21:05:39 +0100

    Content-addressable objects tool

--- files/bash-bindings.sh
+++ files/bash-bindings.sh
@@ -3,7 +3,7 @@
 alias l='ls -CF'
 
 # Alt-Shift-Q = To parent directory
-bind '"\eQ":"\C-a\C-k cd ..\C-j\C-l l\C-j\C-y"'
+#bind '"\eQ":"\C-a\C-k cd ..\C-j\C-l l\C-j\C-y"'
 
 # Alt-Shift-W = Auto-complete and go to sub-directory
-bind '"\eW":"\C-k\C-a cd ./\C-e\t\C-a\C-j\C-l l\C-j\C-y"'
+#bind '"\eW":"\C-k\C-a cd ./\C-e\t\C-a\C-j\C-l l\C-j\C-y"'
