0fc0356ada8b4fdc1f35e69f1f732af2f4f94f3f
Johan Egneblad <johan@egneblad.se>
2017-05-04 07:01:07 +0200

    Simplenote 1.0.8 package

---
+++ packages/simplenote/1.0.8/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DOWNLOAD="https://github.com/Automattic/simplenote-electron/releases/download/v1.0.8/Simplenote-linux-x64.1.0.8.tar.gz"
+TARGZ="simplenote.tar.gz"
+DIRECTORY="Simplenote-linux-x64"
+
+wget --quiet --output-document="$TARGZ" "$DOWNLOAD"
+
+tar xf "$TARGZ"
+rm "$TARGZ"
+
+mv "$DIRECTORY" "$TARGET"
